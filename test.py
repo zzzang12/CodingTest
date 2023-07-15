ac36689994fe12d836315ce631017f3c89bcbb9d
zzzang12 <wadevs12@gmail.com>
2023-07-15 21:45:19 +0900

    230715

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 import sys
-n = int(sys.stdin.readline().strip())
-s = {"ChongChong"}
-for _ in range(n):
-    a, b = sys.stdin.readline().split()
-    if a in s:
-        s.add(b)
-    elif b in s:
-        s.add(a)
-print(len(s))
+while True:
+    lst = list(map(int, sys.stdin.readline().split()))
+    if lst[0] == lst[1] == lst[2] == 0:
+        break
+    lst.sort()
+    if lst[2] >= lst[0] + lst[1]:
+        print("Invalid")
+    elif lst[0] == lst[1] == lst[2]:
+        print("Equilateral")
+    elif lst[0] == lst[1] or lst[1] == lst[2] or lst[0] == lst[2]:
+        print("Isosceles")
+    elif lst[0] != lst[1] != lst[2]:
+        print("Scalene")
