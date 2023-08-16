ace46f90e3c8ad5bb33f00ab097d32fecf1b6730
zzzang12 <wadevs12@gmail.com>
2023-08-16 22:54:13 +0900

    230816

--- test.py
+++ test.py
@@ -1,14 +1,8 @@
 import sys
-while True:
-    lst = list(map(int, sys.stdin.readline().split()))
-    if lst[0] == lst[1] == lst[2] == 0:
-        break
-    lst.sort()
-    if lst[2] >= lst[0] + lst[1]:
-        print("Invalid")
-    elif lst[0] == lst[1] == lst[2]:
-        print("Equilateral")
-    elif lst[0] == lst[1] or lst[1] == lst[2] or lst[0] == lst[2]:
-        print("Isosceles")
-    elif lst[0] != lst[1] != lst[2]:
-        print("Scalene")
+
+n, k = map(int, sys.stdin.readline().split())
+l = [v for v in range(1, n + 1) if n % v == 0]
+if len(l) < k:
+    print(0)
+else:
+    print(l[k - 1])
