51391c07ec9d0ac66c1ddbfea0b1484ec334e510
zzzang12 <wadevs12@gmail.com>
2023-10-23 19:28:17 +0900

    231023

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import sys
 
-int(sys.stdin.readline())
-print("hello")
+n1, n2 = map(int, sys.stdin.readline().split())
+str1 = list(sys.stdin.readline().strip())
+str2 = list(sys.stdin.readline().strip())
+t = int(sys.stdin.readline())
+
+t = min(len(str1) + len(str2) - 1, t)
+str1.reverse()
+str1 = list(" ".join(str1))
+str2 = list(" ".join(str2))
+result = list(" " * (2 * len(str1) + len(str2)))
+
+for i, v in enumerate(str1):
+    result[t * 2 + i] = v
+for i, v in enumerate(str2):
+    if v != " ":
+        result[len(str1) + i] = v
+
+result = "".join(result).replace(" ", "")
+print(result)
