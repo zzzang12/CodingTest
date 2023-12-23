90441f533b3d363ea14b788504862e3fe06e2e10
zzzang12 <wadevs12@gmail.com>
2023-12-24 01:20:15 +0900

    231224

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-import sys
-
-n1, n2 = map(int, sys.stdin.readline().split())
-str1 = list(sys.stdin.readline().strip())
-str2 = list(sys.stdin.readline().strip())
-t = int(sys.stdin.readline())
-
-t = min(len(str1) + len(str2) - 1, t)
-str1.reverse()
-str1 = list(" ".join(str1))
-str2 = list(" ".join(str2))
-result = list(" " * (2 * len(str1) + len(str2)))
-
-for i, v in enumerate(str1):
-    result[t * 2 + i] = v
-for i, v in enumerate(str2):
-    if v != " ":
-        result[len(str1) + i] = v
-
-result = "".join(result).replace(" ", "")
-print(result)
+testcases = int(input())
+for _ in range(testcases):
+  quiz = input()
+  quiz = quiz.split("X")
+  score = [(len(elem) * (len(elem) + 1)) // 2 for elem in quiz if len(elem) != 0]
+  score = sum(score)
+  print(score)
