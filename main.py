62ac3857379a9694d1ecca60bbf755b2c24c9d60
Jangwon Lee <wadevs12@gmail.com>
2024-12-14 00:44:56 +0900

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-testcases = int(input())
-for _ in range(testcases):
-  quiz = input()
-  quiz = quiz.split("X")
-  score = [(len(elem) * (len(elem) + 1)) // 2 for elem in quiz if len(elem) != 0]
-  score = sum(score)
-  print(score)
